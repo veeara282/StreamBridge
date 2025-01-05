e31a6a109d5000f4273ca2cc09fa6dba2c19489d
Evelyn <evelyn.ciara.345@gmail.com>
2025-01-05 15:41:38 -0500

    Set up client and server folders

---
+++ client/client.py
@@ -0,0 +1,4 @@
+# TODO: This will be the client.
+
+if __name__ == "__main__":
+    print("Hello, world!")
---
+++ server/app.py
@@ -0,0 +1,4 @@
+# TODO: This will be the app server.
+
+if __name__ == "__main__":
+    print("Hello, world!")
