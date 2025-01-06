7fff4766ea1adebefeddffc5d4280c801f6a9cf8
Evelyn <evelyn.ciara.345@gmail.com>
2025-01-06 05:00:11 -0500

    Create welcome TwiML endpoint

--- server/app.py
+++ server/app.py
@@ -1,4 +1,20 @@
 # TODO: This will be the app server.
+from flask import Flask, render_template, Response
+
+
+app = Flask(__name__)
+
+
+def twiml_response(body: str):
+    return Response(body, content_type="application/xml")
+
+
+@app.route("/welcome")
+def welcome_message():
+    return twiml_response(
+        render_template("welcome.xml.j2", hostname="https://example.org")
+    )
+
 
 if __name__ == "__main__":
-    print("Hello, world!")
+    app.run(debug=True)
